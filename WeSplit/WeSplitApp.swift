7be0e42e00452b1a48ce78afc48eb459f494eeba
Seth Singletary <singletaryseth8@gmail.com>
2022-04-30 18:07:21 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Seth Singletary on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
